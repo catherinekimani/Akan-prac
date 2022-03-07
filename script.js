2150b6902ce88626bb7f23f2824db242354e38b9
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-07 23:25:16 +0300

    ft: Add changes to form

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function findAkanName () {
 
     // gender variables
     var gender = document.getElementById('input[name="gender"]:checked').value;
+
+    // var days
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+
+    // var Akan names 
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    // birthdate
+    var dateOfBirth = new Date (year + '/' +month + '/' +day);
+    var dayOftheWeek = dateOfBirth.getDay();
 }

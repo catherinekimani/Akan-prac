c4ecfc961983621026673a85b14f435716c3cb1e
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-07 23:26:00 +0300

    ft: fix bugs

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function findAkanName () {
+function getName (){
     // variables: day, month, year.
     var day = document.getElementById("day").value;
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
 
     // gender variables
-    var gender = document.getElementById('input[name="gender"]:checked').value;
+    var gender = document.querySelector('input[name="gender"]:checked').value;
 
     // var days
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
@@ -15,6 +15,19 @@ function findAkanName () {
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
     // birthdate
-    var dateOfBirth = new Date (year + '/' +month + '/' +day);
+    var dateOfBirth = new Date (year + '/'+month + '/' +day);
     var dayOftheWeek = dateOfBirth.getDay();
+
+    // form validation
+    if (day <= 0 || day > 31) {
+        document.getElementById("error").innerText = "Please Enter valid day"
+    } else if (month <= 0 || month > 12) {
+        document.getElementById("error").innerText = "Incorrect!!"
+    } else if (year <= 0 || year > 2022) {
+        document.getElementById("error").innerText = "Please enter valid year"
+    } else if(gender == "male") {
+        document.getElementById("findName").innerText = "your were born on " + days[dayOftheWeek] + "." + " Your Akan name is " + maleNames[dayOftheWeek] + "!"
+    } else if (gender == "female"){
+        document.getElementById("findName").innerText = "your were born on" + days[dayOftheWeek] + "." + " Your Akan name is " + femaleNames[dayOftheWeek] + "!"
+    }
 }

446eee3b6e7e9b5eb2e5a10beb1c2d4a3531b6d8
catherinekimani <catherine.kimani@student.moringaschool.com>
2022-03-07 18:07:07 +0300

    Add variables

---
+++ script.js
@@ -0,0 +1,9 @@
+function findAkanName () {
+    // variables: day, month, year.
+    var day = document.getElementById("day").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
+
+    // gender variables
+    var gender = document.getElementById('input[name="gender"]:checked').value;
+}
